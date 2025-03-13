14615c1947ec1837eecda704f8b1fd43befe72bd
reneecwl <reneewlcheng@gmail.com>
2025-03-13 13:57:34 -0400

    Append weather data to planning page

--- client/src/components/ItineraryOutline/ItineraryOutline.jsx
+++ client/src/components/ItineraryOutline/ItineraryOutline.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 export default function ItineraryOutline({ itinerary }) {
-  const [weather, setWeather] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
   const baseUrlWeather = import.meta.env.VITE_WEATHER_API_URL;
   const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
@@ -18,8 +18,7 @@ export default function ItineraryOutline({ itinerary }) {
     const fetchWeather = async () => {
       try {
         const response = await axios.get(`${baseUrlWeather}${apiKey}&q=${itinerary.location}&days=5`);
-        console.log(response);
-        setWeather(response.data);
+        setWeatherData(response.data.forecast);
       } catch (error) {
         console.error("There is an error loading the weather", error);
       }
@@ -27,10 +26,18 @@ export default function ItineraryOutline({ itinerary }) {
     fetchWeather();
   }, []);
 
-  if (!weather) {
+  if (!weatherData) {
     return <div>Loading...</div>;
   }
-  console.log(weather);
+  console.log(weatherData);
+
+  const extractedData = weatherData.forecastday.map((day) => ({
+    date: day.date,
+    mintemp_c: day.day.mintemp_c,
+    maxtemp_c: day.day.maxtemp_c,
+    daily_chance_of_rain: day.day.daily_chance_of_rain,
+    icon: day.day.condition.icon, // Include the weather icon
+  }));
 
   return (
     <>
@@ -38,8 +45,20 @@ export default function ItineraryOutline({ itinerary }) {
 
       <p> {itinerary.itinerary_name || `${diferenceInDays} Days trip in ${itinerary.location}`}</p>
       <p>
-        {formattedStartDate} -{formattedEndDate}{" "}
+        {" "}
+        {formattedStartDate} -{formattedEndDate}
       </p>
+      <ul>
+        {extractedData.map((day, index) => (
+          <li key={index}>
+            <p>Date: {day.date}</p>
+            <p>Min Temp: {day.mintemp_c}°C</p>
+            <p>Max Temp: {day.maxtemp_c}°C</p>
+            <p>Chance of Rain: {day.daily_chance_of_rain}%</p>
+            <img src={`https:${day.icon}`} alt="Weather Icon" /> {/* Display the weather icon */}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
