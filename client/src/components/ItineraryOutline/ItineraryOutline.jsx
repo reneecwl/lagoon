3b94b5ade37ea0ff2397552be9eb8df4f1c62899
reneecwl <reneewlcheng@gmail.com>
2025-03-13 14:24:50 -0400

    Fix daysdifference function

--- client/src/components/ItineraryOutline/ItineraryOutline.jsx
+++ client/src/components/ItineraryOutline/ItineraryOutline.jsx
@@ -3,6 +3,7 @@ import FormatDate from "../../utility/FormatDate";
 import DaysDifference from "../../utility/DaysDifference";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { differenceInDays } from "date-fns";
 
 export default function ItineraryOutline({ itinerary }) {
   const [weatherData, setWeatherData] = useState(null);
@@ -11,8 +12,10 @@ export default function ItineraryOutline({ itinerary }) {
 
   const formattedStartDate = FormatDate(itinerary.start_date);
   const formattedEndDate = FormatDate(itinerary.end_date);
-
   const diferenceInDays = DaysDifference(formattedStartDate, formattedEndDate);
+
+  console.log(`start date: ${formattedStartDate}`);
+  console.log(differenceInDays);
 
   useEffect(() => {
     const fetchWeather = async () => {
@@ -33,10 +36,10 @@ export default function ItineraryOutline({ itinerary }) {
 
   const extractedData = weatherData.forecastday.map((day) => ({
     date: day.date,
-    mintemp_c: day.day.mintemp_c,
-    maxtemp_c: day.day.maxtemp_c,
+    mintemp_c: Math.round(day.day.mintemp_c),
+    maxtemp_c: Math.round(day.day.maxtemp_c),
     daily_chance_of_rain: day.day.daily_chance_of_rain,
-    icon: day.day.condition.icon, // Include the weather icon
+    icon: day.day.condition.icon,
   }));
 
   return (
@@ -51,11 +54,12 @@ export default function ItineraryOutline({ itinerary }) {
       <ul>
         {extractedData.map((day, index) => (
           <li key={index}>
-            <p>Date: {day.date}</p>
-            <p>Min Temp: {day.mintemp_c}°C</p>
-            <p>Max Temp: {day.maxtemp_c}°C</p>
+            <p>{day.date}</p>
+            <p>
+              {day.mintemp_c} - {day.maxtemp_c} °C
+            </p>
             <p>Chance of Rain: {day.daily_chance_of_rain}%</p>
-            <img src={`https:${day.icon}`} alt="Weather Icon" /> {/* Display the weather icon */}
+            <img src={`https:${day.icon}`} alt="Weather Icon" />
           </li>
         ))}
       </ul>
