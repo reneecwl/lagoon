de3c19d9869f0982b6f0359104e41c9234cd714c
reneecwl <reneewlcheng@gmail.com>
2025-03-13 20:47:05 -0400

    Convert api date to local time

--- client/src/components/ItineraryOutline/ItineraryOutline.jsx
+++ client/src/components/ItineraryOutline/ItineraryOutline.jsx
@@ -3,10 +3,10 @@ import FormatDate from "../../utility/FormatDate";
 import DaysDifference from "../../utility/DaysDifference";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { differenceInDays } from "date-fns";
 
 export default function ItineraryOutline({ itinerary }) {
   const [weatherData, setWeatherData] = useState(null);
+  const [filteredWeatherData, setFilteredWeatherData] = useState([]);
   const baseUrlWeather = import.meta.env.VITE_WEATHER_API_URL;
   const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
@@ -14,27 +14,39 @@ export default function ItineraryOutline({ itinerary }) {
   const formattedEndDate = FormatDate(itinerary.end_date);
   const diferenceInDays = DaysDifference(formattedStartDate, formattedEndDate);
 
-  console.log(`start date: ${formattedStartDate}`);
-  console.log(differenceInDays);
-
   useEffect(() => {
     const fetchWeather = async () => {
       try {
-        const response = await axios.get(`${baseUrlWeather}${apiKey}&q=${itinerary.location}&days=5`);
+        const response = await axios.get(`${baseUrlWeather}${apiKey}&q=${itinerary.location}&days=14`);
         setWeatherData(response.data.forecast);
+
+        const startDateObj = new Date(itinerary.start_date);
+        const endDateObj = new Date(itinerary.end_date);
+
+        // startDateObj.setHours(0, 0, 0, 0);
+        // endDateObj.setHours(23, 59, 59, 999);
+
+        console.log(response.data.forecast.forecastday);
+        const filteredData = response.data.forecast.forecastday.filter((day) => {
+          const dayDateObj = new Date(day.date + "T00:00:00");
+          console.log("Day Date:", day.date, dayDateObj);
+
+          return dayDateObj >= startDateObj && dayDateObj <= endDateObj;
+        });
+
+        setFilteredWeatherData(filteredData);
       } catch (error) {
         console.error("There is an error loading the weather", error);
       }
     };
     fetchWeather();
-  }, []);
+  }, [itinerary.location, itinerary.start_date, itinerary.end_date]);
 
   if (!weatherData) {
     return <div>Loading...</div>;
   }
-  console.log(weatherData);
 
-  const extractedData = weatherData.forecastday.map((day) => ({
+  const extractedData = filteredWeatherData.map((day) => ({
     date: day.date,
     mintemp_c: Math.round(day.day.mintemp_c),
     maxtemp_c: Math.round(day.day.maxtemp_c),
@@ -47,14 +59,14 @@ export default function ItineraryOutline({ itinerary }) {
       <h5> This is the itinerary outline component</h5>
 
       <p> {itinerary.itinerary_name || `${diferenceInDays} Days trip in ${itinerary.location}`}</p>
-      <p>
+      <h4>
         {" "}
-        {formattedStartDate} -{formattedEndDate}
-      </p>
+        {formattedStartDate} to {formattedEndDate}
+      </h4>
       <ul>
         {extractedData.map((day, index) => (
           <li key={index}>
-            <p>{day.date}</p>
+            <h4>{day.date}</h4>
             <p>
               {day.mintemp_c} - {day.maxtemp_c} °C
             </p>
--- client/src/utility/FormatDate.jsx
+++ client/src/utility/FormatDate.jsx
@@ -1,5 +1,5 @@
 import { format } from "date-fns";
 
 export default function FormatDate(dateString) {
-  return format(new Date(dateString), "yyyy/MM/dd");
+  return format(new Date(dateString), "yyyy-MM-dd");
 }
