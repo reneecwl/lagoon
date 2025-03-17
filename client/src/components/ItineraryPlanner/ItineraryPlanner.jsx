d1e850d46fd93e3f1f2f111ea63ce140cecc2cf4
reneecwl <reneewlcheng@gmail.com>
2025-03-17 18:38:57 -0400

    Update attraction seeds

--- client/src/components/ItineraryPlanner/ItineraryPlanner.jsx
+++ client/src/components/ItineraryPlanner/ItineraryPlanner.jsx
@@ -1,18 +1,16 @@
 import "./ItineraryPlanner.scss";
 
 export default function ItineraryPlanner({ dailyAttractions }) {
-  // console.log(`Daily Attractions:`, dailyAttractions);
-
   return (
     <div className="planner">
       {dailyAttractions.map((attractionByDay, index) => (
         <div key={index} className="planner__byday">
-          <h3 className="planner_day"> Day {attractionByDay.day}:</h3>
+          <h3 className="planner__dayIndex"> Day {attractionByDay.day}:</h3>
           {attractionByDay.attractions.map((attraction) => (
             <div key={attraction.id} className="planner__attraction">
-              <p className="">{attraction.attraction_name}</p>
-              <p className="">Suggested Duration: {attraction.suggested_duration}</p>
-              <p className="">User Notes: {attraction.user_notes}</p>
+              <p className="planner__attraction-name">{attraction.attraction_name}</p>
+              <p className="planner__duration">Suggested Duration: {attraction.suggested_duration}</p>
+              <p className="planner__notes">User Notes: {attraction.user_notes}</p>
             </div>
           ))}
         </div>
